2f2177678e18bca0544ee2d2999bc8a167cf0e64
Marco Acea <aceamarco@gmail.com>
2024-07-25 21:33:01 -0400

    updating vite config" "

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 
+  base: "/my-json-portfolio/",
+
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
+      "@": path.resolve(__dirname, "src"),
+    },
   },
 
   server: {
